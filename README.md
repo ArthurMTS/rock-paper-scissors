35b580c0952afeca422b6ae2e29e5fecea05cbbe
Arthur Silva <arthurmatsilva@outlook.com>
2022-02-10 14:16:29 -0300

    refactor: making the code a little better

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Rock, Paper, Scissors game.
 
+<img src="https://i.pinimg.com/originals/e1/52/1b/e1521b047cddd606ff9c44af3eb6e324.jpg" alt="Project screen" />
+
 Rock, paper, scissors game for [The Odin Project](https://www.theodinproject.com/dashboard).
 
 [Live demo](https://arthurmts.github.io/rock-paper-scissors/index.html).
--- js/main.js
+++ js/main.js
@@ -1,90 +1,62 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', e => playRound(e.target.textContent)));
-
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection = '';
-let computerSelection = '';
+const results = [
+  ["Tie", "You lose", "You win"],
+  ["You win", "Tie", "You lose"],
+  ["You lose", "You win", "Tie"],
+];
 
 function computerPlay() {
-    switch (Math.floor(Math.random() * 3)) {
-        case 0:
-            return 'rock'
-        case 1:
-            return 'paper'
-        case 2:
-            return 'scissors'
-    }
+  return (Math.floor(Math.random() * 3));
 }
 
 function updateScore() {
-    const score = document.querySelector('#score strong');
+  const score = document.querySelector("#score strong");
+  score.textContent = `${playerScore} - ${computerScore}`;
 
-    score.textContent = `${playerScore} - ${computerScore}`;
-
-    if (playerScore > 4 || computerScore > 4) {
-        reloadButton();
-    }
+  if (playerScore >= 5 || computerScore >= 5)
+    showReloadButton();
 }
 
-function updateMoves(playerSelection, computerSelection) {
-    const player = document.querySelector('div#player ul');
-    const computer = document.querySelector('div#computer ul');
+function updateMoves(playerMove, computerMove) {
+  const moves = ["Rock", "Paper", "Scissors"];
+  const playerMoves = document.querySelector("div#player ul");
+  const computerMoves = document.querySelector("div#computer ul");
     
-    const playerMove = document.createElement('li');
-    playerMove.textContent = playerSelection;
-    const computerMove = document.createElement('li');
-    computerMove.textContent = computerSelection;
+  const playerPlay = document.createElement("li");
+  playerPlay.textContent = moves[playerMove];
+  const computerPlay = document.createElement("li");
+  computerPlay.textContent = moves[computerMove];
 
-    player.appendChild(playerMove);
-    computer.appendChild(computerMove);
+  playerMoves.appendChild(playerPlay);
+  computerMoves.appendChild(computerPlay);
 }
 
-function updateInfo(resultText) {
-    const result = document.querySelector('#result p');
-    result.textContent = resultText;
-}
-
-function reloadButton() {
-    const result = document.querySelector('#result');
+function showReloadButton() {
+  const result = document.querySelector("#result");
     
-    const button = document.createElement('button');
-    button.textContent = 'Play again';
-    button.addEventListener('click', () => location.reload());
+  const button = document.createElement("button");
+  button.textContent = "Play again";
+  button.addEventListener("click", () => location.reload());
 
-    result.appendChild(button);
+  result.appendChild(button);
 }
 
-function playRound(playerSelection) {
-    const computerSelection = computerPlay();
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerMove) {
+  const computerMove = computerPlay();
 
-    if (playerScore > 4 || computerScore > 4) return;
+  if (playerScore >= 5 || computerScore >= 5) return;
 
-    if (playerSelection === computerSelection) {
-        updateInfo('Tie');
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        playerScore++;
-        updateInfo('You win! Rock beats Scissors.')
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        computerScore++;
-        updateInfo('You lose! Paper beats Rock.');
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        updateInfo('You win! Paper beats Rock.');
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore++;
-        updateInfo('You lose! Scissors beats Paper.');
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        updateInfo('You win! Scissors beats Paper.');
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore++;
-        updateInfo('You lose! Rock beats Scissors.');
-    } else {
-        updateInfo('Hm... something is not right.');
-    }
+  if (results[playerMove][computerMove] === "You win")
+    playerScore++;
+  else if (results[playerMove][computerMove] === "You lose")
+    computerScore++;
 
-    updateScore();
-    updateMoves(playerSelection, computerSelection);
+  updateScore();
+  updateMoves(playerMove, computerMove);
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button, key) => 
+  button.addEventListener("click", () => playRound(key))
+);
